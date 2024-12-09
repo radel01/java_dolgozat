68a225d5ea409e9a63b6cdf671b3df58493648b3
radel01 <roszkopf.adel@gmail.com>
2024-12-09 13:46:12 +0100

    f1 4

--- src/main/java/szamalk/hu/Main.java
+++ src/main/java/szamalk/hu/Main.java
@@ -2,6 +2,7 @@ package szamalk.hu;
 
 public class Main {
     public static void main(String[] args) {
-
+        Tábla Tábla=new Tábla('*');
+        Tábla.Megjelenít();
     }
 }
--- src/main/java/szamalk/hu/Tábla.java
+++ src/main/java/szamalk/hu/Tábla.java
@@ -4,7 +4,7 @@ public class Tábla {
     private Character ÜresCella;
     private Character[][] T;
 
-    public Tábla(Character üresCella, Character[][] t) {
+    public Tábla(Character üresCella) {
         this.ÜresCella= üresCella;
         T =new Character[8][8] ;
 
@@ -35,7 +35,14 @@ public class Tábla {
     }
 
     public void Megjelenít(){
-
+        for( int i = 0; i < T.length; i++ )
+        {
+            for( int j = 0; j < T[i].length; j++ )
+            {
+                System.out.print(T[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 
     public void ÜresOszlop(){
