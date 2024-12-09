806fd7ba1b5d76ad03a54ce795a1ad445b38b34c
radel01 <roszkopf.adel@gmail.com>
2024-12-09 13:40:28 +0100

    f1 3

--- src/main/java/szamalk/hu/Tábla.java
+++ src/main/java/szamalk/hu/Tábla.java
@@ -4,6 +4,46 @@ public class Tábla {
     private Character ÜresCella;
     private Character[][] T;
 
-   
+    public Tábla(Character üresCella, Character[][] t) {
+        this.ÜresCella= üresCella;
+        T =new Character[8][8] ;
+
+        for( int i = 0; i < T.length; i++ )
+        {
+            for( int j = 0; j < T[i].length; j++ )
+            {
+                T[i][j] = ÜresCella;
+            }
+        }
+
+    }
+
+    public void ÜresOszlopokSzáma(){
+
+    }
+
+    public void ÜresSorokSzáma(){
+
+    }
+
+    public void Elhelyez(){
+
+    }
+
+    public void FájlbaÍr(){
+
+    }
+
+    public void Megjelenít(){
+
+    }
+
+    public void ÜresOszlop(){
+
+    }
+
+    public void ÜresSor(){
+
+    }
 
 }
