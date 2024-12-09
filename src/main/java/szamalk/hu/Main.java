1232a23d87d1023971d9c6d0b08b97f78136e3ba
radel01 <roszkopf.adel@gmail.com>
2024-12-09 13:57:14 +0100

    f1 6

--- src/main/java/szamalk/hu/Main.java
+++ src/main/java/szamalk/hu/Main.java
@@ -3,7 +3,7 @@ package szamalk.hu;
 public class Main {
     public static void main(String[] args) {
         Tábla Tábla=new Tábla('*');
-        
+        Tábla.Elhelyez(8);
         Tábla.Megjelenít();
 
     }
